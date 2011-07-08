cf7061875131a3c85bead6dfbcfb0f6e3239a294
Hariharan Jayaram <harijay@gmail.com>
2011-07-08 19:39:35 -0400

    Initial commit with rechecking popup

--- spynner-agent.py
+++ spynner-agent.py
@@ -3,18 +3,25 @@ import spynner
 from spynner import browser
 import pyquery
 import pynotify
-
+import time
+import private
 
 agent = browser.Browser()
 # agent.create_webview(True)
 # agent.show()
 agent.load("https://webmail.constellationpharma.com")
-agent.fill("input[name=username]","hari.jayaram")
-agent.fill("input[name=password]","Eer101cer101")
+agent.fill("input[name=username]",private.uname)
+agent.fill("input[name=password]",private.password)
 agent.click("input[type=submit]")
-agent.wait_load()
-d = pyquery.PyQuery(agent.html)
-p = pynotify.Notification("New email title: " + str(d(".frst a"))).show()
+agent.create_webview(True)
 
+for i in range(10):
+    time.sleep(300)
+#    agent.show()
+    agent.click("a[title=Check Messages]")
+    agent.wait_load()
+    d = pyquery.PyQuery(agent.html)
+    time.sleep(5)
+    p = pynotify.Notification("New email title: " + str(d(".frst a"))).show()
 
 
